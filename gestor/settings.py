3eec5cdcba19130e73be9464b46c859ba356d2ca
excel-ente <turkienich@gmail.com>
2023-05-06 12:03:28 -0300

    Cambios de user 2

--- gestor/settings.py
+++ gestor/settings.py
@@ -75,14 +75,11 @@ WSGI_APPLICATION = 'gestor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ZETA123',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
